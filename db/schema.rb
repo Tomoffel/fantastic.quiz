613763471678e0514038fe33ff2897d9475b951e
Tomoffel <jw622290@fh-muenster.de>
2014-12-29 14:50:34 +0100

    Change Databaseschema

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141229122330) do
+ActiveRecord::Schema.define(version: 20141229113707) do
 
   create_table "answers", force: true do |t|
     t.text     "answer"
