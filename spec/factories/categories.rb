bae52119e50a4345bc87cf2da256caa55a1db601
Tomoffel <jw622290@fh-muenster.de>
2014-12-29 13:03:10 +0100

    add testcases for category

--- spec/factories/categories.rb
+++ spec/factories/categories.rb
@@ -1,7 +1,17 @@
 FactoryGirl.define do
   factory :category do
-    name "MyString"
-parent nil
+    name "Testcategory"
+    parent_id nil
+
+    factory :categoryWithParent do
+      name "Testcategory2"
+      parent_id 1
+
+      factory :categoryWithoutName do
+        name nil
+        parent_id 1
+      end
+    end
   end
 
 end
--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe Category, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+  let!(:category) { FactoryGirl.build(:category) }
+  let!(:categoryWParent) { FactoryGirl.build(:categoryWithParent) }
+  let!(:categoryWoName) { FactoryGirl.build(:categoryWithoutName) }
+
+  context 'test valid categories' do
+    it 'is valid without parent' do
+      expect(category).to be_valid
+    end
+    it 'is valid with parent' do
+      expect(categoryWParent).to be_valid
+    end
+  end
+
+  context 'test invalid categories' do
+    it 'is invalid without name' do
+      expect(categoryWoName).to_not be_valid
+    end
+
+  end
+
 end
