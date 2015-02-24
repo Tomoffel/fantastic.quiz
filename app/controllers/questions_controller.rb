580c361cb41bcdb0937453aa91b8445e7c3d8a2d
Tomoffel <jw622290@fh-muenster.de>
2015-02-24 14:18:50 +0100

    Fix Error

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -208,7 +208,7 @@ class QuestionsController < ApplicationController
     ownCategories = Array.new
 
     @question.categories.each do |cat|
-      if current_user.has_role?(cat.name) || current_user.has_role?(:admin)
+      if current_user.has_role?(cat.name + cat.id.to_s) || current_user.has_role?(:admin)
         ownCategories.push(cat)
       end
     end
